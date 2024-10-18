b45232dea3cdb153681182f8345b7b5323dba361
Rushikesh Chaudhari <122673672+rushi055@users.noreply.github.com>
2024-10-18 14:31:24 +0530

    commited

---
+++ Zoom clone/lib/screens/history_meeting_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../resources/firestore_methods.dart';
+
+class HistoryMeetingScreen extends StatelessWidget {
+  const HistoryMeetingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirestoreMethods().meetingsHistory,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: (snapshot.data! as dynamic).docs.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(
+              'Room Name: ${(snapshot.data! as dynamic).docs[index]['meetingName']}',
+            ),
+            subtitle: Text(
+              'Joined on ${DateFormat.yMMMd().format((snapshot.data! as dynamic).docs[index]['createdAt'].toDate())}',
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ Zoom clone/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import cloud_firestore
+import firebase_auth
+import firebase_core
+import google_sign_in_ios
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+}
