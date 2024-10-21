3f2f02a7a11016e83cfb237815ac4e794611da56
Anuj <singhanuj620@gmail.com>
2024-10-21 23:24:43 +0530

    feat(javascript_quiz) : added a small project for js quiz

---
+++ Javascript Quiz/src/Components/index.js
@@ -0,0 +1,4 @@
+export { default as LandingPage } from './LandingPage/LandingPage'
+export { default as UserInfo } from './UserInfo/UserInfo'
+export { default as Questions } from './Questions/Questions'
+export { default as Result } from './Result/Result'
