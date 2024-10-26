bfb897b668ed8ec5178e366594357f24e83e3890
Sahil Arora <sahil.2723.a@gmail.com>
2024-10-26 12:17:49 +0530

    Zod Validation

---
+++ Zod validation/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RegistrationForm from './RegistrationForm'
+
+const App = () => {
+  return (
+    <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px' }}>
+      <h1>Registration Form</h1>
+      <RegistrationForm />
+    </div>
+  )
+}
+
+export default App
