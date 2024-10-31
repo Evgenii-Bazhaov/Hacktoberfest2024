069f46518da35c4a3028a4eacf70c6aba07290f0
HITANSHU SINGH <122127354+2002hitanshu@users.noreply.github.com>
2024-10-31 20:33:08 +0530

    Create chatterbot.py

---
+++ chatterbot.py
@@ -0,0 +1,13 @@
+# bot.py
+
+from chatterbot import ChatBot
+
+chatbot = ChatBot("Chatpot")
+
+exit_conditions = (":q", "quit", "exit")
+while True:
+    query = input("> ")
+    if query in exit_conditions:
+        break
+    else:
+        print(f"🪴 {chatbot.get_response(query)}")
